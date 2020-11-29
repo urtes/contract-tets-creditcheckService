0d0103e250dced70d280e3c9e582676159bd33ea
Urte <urte.rimsaite@gmail.com>
2020-11-29 16:37:53 +0200

    Initial commit

---
+++ src/main/java/com/retailbank/creditcheckService/CreditcheckServiceApplication.java
@@ -0,0 +1,13 @@
+package com.retailbank.creditcheckService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreditcheckServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CreditcheckServiceApplication.class, args);
+	}
+
+}
